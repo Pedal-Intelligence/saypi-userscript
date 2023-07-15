4b5d0b74f31855599ed98a3f8e6324ac68d042ea
Ross Cadogan <ross.cadogan@gmail.com>
2023-07-15 22:05:26 +0100

    Adjust styling for expanded speaker ctrl

--- saypi.js
+++ saypi.js
@@ -8,11 +8,11 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
 
     // Create a MutationObserver to listen for changes to the DOM
-    var observer = new MutationObserver(function(mutations) {
+    var observer = new MutationObserver(function (mutations) {
         // Check each mutation
         for (var i = 0; i < mutations.length; i++) {
             var mutation = mutations[i];
@@ -28,10 +28,10 @@
 
                         // Get the button and register for mousedown and mouseup events
                         var button = document.getElementById('talkButton');
-                        button.addEventListener('mousedown', function() {
+                        button.addEventListener('mousedown', function () {
                             console.log('Button pressed');
                         });
-                        button.addEventListener('mouseup', function() {
+                        button.addEventListener('mouseup', function () {
                             console.log('Button released');
                         });
 
@@ -40,7 +40,7 @@
                         button.style.float = 'right';
                         button.style.width = '50px';
                         button.style.height = '50px';
-                        button.style.marginRight = '75px';
+                        button.style.marginRight = '100px';
                         button.style.border = '1px solid';
 
                         observer.disconnect();
