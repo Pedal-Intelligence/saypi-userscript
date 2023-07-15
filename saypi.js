1a814510f79c83f0fb4ad7d9861a97a4de68b026
Ross Cadogan <ross.cadogan@gmail.com>
2023-07-15 20:12:31 +0100

    Add basic "Talk" button to page

--- saypi.js
+++ saypi.js
@@ -11,12 +11,28 @@
 (function () {
     'use strict';
 
-    // Find the first <audio> element
-    var audioElement = document.querySelector('audio');
-    if (audioElement) {
-        // Insert a new <button> element just before the <audio> element
-        audioElement.insertAdjacentHTML('beforebegin', '<button id="talkButton">Talk</button>');
-    } else {
-        console.log('No <audio> element found');
-    }
+    // Create a MutationObserver to listen for changes to the DOM
+    var observer = new MutationObserver(function (mutations) {
+        // Check each mutation
+        for (var i = 0; i < mutations.length; i++) {
+            var mutation = mutations[i];
+
+            // If nodes were added, check each one
+            if (mutation.addedNodes.length > 0) {
+                for (var j = 0; j < mutation.addedNodes.length; j++) {
+                    var node = mutation.addedNodes[j];
+
+                    // If the node is an <audio> element, add the button and stop observing
+                    if (node.nodeName.toLowerCase() === 'audio') {
+                        node.insertAdjacentHTML('beforebegin', '<button id="talkButton">Talk</button>');
+                        observer.disconnect();
+                        return;
+                    }
+                }
+            }
+        }
+    });
+
+    // Start observing the entire document for changes to child nodes and subtree
+    observer.observe(document, { childList: true, subtree: true });
 })();
