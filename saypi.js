1b8e1c1afba999612a743e86eca1a34736c71e55
Ross Cadogan <ross.cadogan@gmail.com>
2023-07-15 16:56:12 +0100

    Initial - Hello, Pi

---
+++ saypi.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Say, Pi
+// @namespace    http://veloware.com/
+// @version      0.1
+// @description  Speak to Pi with OpenAI's Whisper
+// @author       Ross Cadogan
+// @match        https://heypi.com/*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    console.log("Hello, Pi!");
+})();
