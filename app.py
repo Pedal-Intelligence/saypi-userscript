4599af56bea7859cec033d860d6b10cf0e61f9a6
Ross Cadogan <ross.cadogan@gmail.com>
2023-07-16 17:46:26 +0100

    Simplify remote call

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
 from flask import Flask, request, jsonify
-import requests
-import json
 import os
+import openai
 from flask_cors import CORS
-import base64
 
 app = Flask(__name__)
 CORS(app)
 # CORS(app, origins="https://heypi.com")
-
-# Load your OpenAI API key from an environment variable
-api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
 @app.route("/transcribe", methods=["POST"])
@@ -18,50 +14,12 @@ def transcribe():
     # Load the audio file from the POST request
     audio_file = request.files["audio"]
 
-    # Read the audio file into bytes
-    audio_bytes = audio_file.read()
-
-    # Base64 encode the bytes
-    audio_base64bytes = base64.b64encode(audio_bytes)
-
-    # Convert bytes to string
-    audio_str = audio_base64bytes.decode("utf-8")
-
-    # Now use `audio_str` in your data dict
-    data = {
-        "audio": {
-            "data": audio_str,
-        },
-        "config": {
-            "encoding": "LINEAR16",
-            "sample_rate_hertz": 16000,
-        },
-        "model": "whisper-1",
-    }
-
-    headers = {
-        "Authorization": f"Bearer {os.getenv('OPENAI_API_KEY')}",
-        "Content-Type": "application/json",
-    }
-
-    response = requests.post(
-        "https://api.openai.com/v1/audio/transcriptions",
-        headers=headers,
-        data=json.dumps(data),
-    )
-
-    if response.status_code == 200:
-        return jsonify(response.json())
-    else:
-        return (
-            jsonify(
-                {
-                    "error": "Failed to transcribe audio",
-                    "openai_response": response.json(),
-                }
-            ),
-            500,
-        )
+    # Use the OpenAI Python library to transcribe the audio file
+    try:
+        transcript = openai.Audio.transcribe("whisper-1", audio_file)
+        return transcript
+    except Exception as e:
+        return jsonify({"error": "Failed to transcribe audio", "details": str(e)}), 500
 
 
 @app.after_request
