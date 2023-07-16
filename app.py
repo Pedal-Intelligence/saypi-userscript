b37d63b6d528bfa2cc98c65f69c16ee637302d47
Ross Cadogan <ross.cadogan@gmail.com>
2023-07-16 11:23:14 +0100

    Backend app for CORS requirements

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, jsonify
+import requests
+import json
+
+app = Flask(__name__)
+
+# Load your OpenAI API key from an environment variable or secret management service
+api_key = 'your_openai_api_key'
+
+@app.route('/transcribe', methods=['POST'])
+def transcribe():
+    audio_data = request.get_data()
+    headers = {
+        'Authorization': f'Bearer {api_key}',
+        'Content-Type': 'application/json',
+    }
+
+    # The Whisper API expects a file object, so we need to save the audio data to a file
+    with open('audio.webm', 'wb') as audio_file:
+        audio_file.write(audio_data)
+
+    data = {
+        'file': open('audio.webm', 'rb'),
+        'model': 'whisper-1',
+    }
+
+    response = requests.post('https://api.openai.com/v1/audio/transcriptions', headers=headers, data=json.dumps(data))
+
+    if response.status_code == 200:
+        return jsonify(response.json())
+    else:
+        return jsonify({'error': 'Failed to transcribe audio'}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
