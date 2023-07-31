b42a6ce4c944e64c53a25ed66805fce509401ab9
Ross Cadogan <ross.cadogan@gmail.com>
2023-07-31 15:58:40 +0100

    URL rewriting for trusted links

--- server.js
+++ server.js
@@ -1,30 +1,44 @@
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
+const cheerio = require('cheerio');
 const serveHandler = require('serve-handler');
+const url = require('url');
+const axios = require('axios');
 
 const app = express();
 
-// Handle '/saypi.user.js' specifically
 app.get('/saypi.user.js', (req, res) => {
     serveHandler(req, res, {
-        public: 'public', // directory to serve
+        public: 'public',
     });
 });
 
-app.get(['/', '/index.html'], createProxyMiddleware({
-    target: 'https://saypi.my.canva.site/',
-    changeOrigin: true,
-    followRedirects: true
-}));
+app.all(['/', '/index.html'], async (req, res) => {
+    try {
+        const response = await axios.get('https://saypi.my.canva.site/', { responseType: 'text' });
+        const $ = cheerio.load(response.data);
 
-// Check if it's not a local resource
-app.get('/static/*', (req, res) => {
+        // Find the link and change its href
+        $('a').each((index, element) => {
+            const href = $(element).attr('href');
+            if (href && url.parse(href).pathname === '/_link/' && href.includes('saypi.user.js')) {
+                $(element).attr('href', '/saypi.user.js');
+            }
+        });
+
+        // Send the modified HTML
+        res.send($.html());
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+});
+
+app.use('/static', (req, res) => {
     serveHandler(req, res, {
-        public: 'public', // directory to serve
+        public: 'public',
     });
 });
 
-// Proxy any other request to Canva
 app.use('*', createProxyMiddleware({
     target: 'https://saypi.my.canva.site/',
     changeOrigin: true,
