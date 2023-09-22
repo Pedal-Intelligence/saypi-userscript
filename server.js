59d9cfde587a5eae417cffe905228633b8b82c04
Ross Cadogan <ross.cadogan@gmail.com>
2023-09-22 21:29:36 +0100

    Production env deployment code
    - server.js had become overfit to localhost development
    - add back in rules for production on Heroku
    Ignore self-signed SSL certificates in prod, etc.

--- server.js
+++ server.js
@@ -1,39 +1,52 @@
 const fs = require("fs");
+const http = require("http");
 const https = require("https");
 const express = require("express");
 const path = require("path");
 
-// Read the key and certificate files
-const privateKey = fs.readFileSync(
-  path.join(__dirname, "certificates/localhost-key.pem"),
-  "utf8"
-);
-const certificate = fs.readFileSync(
-  path.join(__dirname, "certificates/localhost.pem"),
-  "utf8"
-);
-
-// Create a credentials object
-const credentials = { key: privateKey, cert: certificate };
+const isProduction = process.env.NODE_ENV === "production";
+let server;
 
 const app = express();
 
+// Allowed origins for CORS
+const allowedOrigins = [
+  "https://pi.ai",
+  "https://www.saypi.ai",
+  "https://app.saypi.ai",
+];
+
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://pi.ai");
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
   next();
 });
 
 app.use("/", express.static(path.join(__dirname, "public")));
 
-// Create an HTTPS service with the Express app
-const httpsServer = https.createServer(credentials, app);
+if (!isProduction) {
+  const privateKey = fs.readFileSync(
+    path.join(__dirname, "certificates/localhost-key.pem"),
+    "utf8"
+  );
+  const certificate = fs.readFileSync(
+    path.join(__dirname, "certificates/localhost.pem"),
+    "utf8"
+  );
+
+  const credentials = { key: privateKey, cert: certificate };
+  server = https.createServer(credentials, app);
+} else {
+  server = http.createServer(app);
+}
 
 if (require.main === module) {
-  // This means this file was run directly from command line, so start the server
-  httpsServer.listen(process.env.PORT || 4443, "0.0.0.0", () => {
-    console.log(`App server listening on port ${process.env.PORT || 4443}`);
+  const port = process.env.PORT || (isProduction ? 80 : 4443);
+  server.listen(port, "0.0.0.0", () => {
+    console.log(`App server listening on port ${port}`);
   });
 } else {
-  // This file was required from another module, so export the app without starting the server
   module.exports = app;
 }
