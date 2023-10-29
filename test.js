2462c4017770283e50b6112611181d32379a2675
Ross Cadogan <ross.cadogan@gmail.com>
2023-10-29 21:47:04 +0000

    Force quit test suite
    - GET userscript test case passes but does not exit
    - Force quit (--exit)
    - It's a hack, cause of non-exit unknown
    - Add test for new audio module dependency

--- test.js
+++ test.js
@@ -1,10 +1,19 @@
 import request from "supertest";
 import app from "./server.js"; // make sure to export your app in server.js
 
-describe("GET /saypi.user.js", function () {
+describe("GET Say, Pi userscript", function () {
   it("responds with js", function (done) {
     request(app)
       .get("/saypi.user.js")
+      .expect("Content-Type", /javascript/)
+      .expect(200, done);
+  });
+});
+
+describe("GET audio module bundle", function () {
+  it("responds with js", function (done) {
+    request(app)
+      .get("/audioModule.bundle.js")
       .expect("Content-Type", /javascript/)
       .expect(200, done);
   });
