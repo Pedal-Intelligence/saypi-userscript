db8eed4683e4e0b952806f89cf5b2efc29f50c78
Ross Cadogan <ross.cadogan@gmail.com>
2023-11-29 15:59:30 +0000

    Styled popup
    - clicking on an icon selects the corresponding slider position

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,9 +1,49 @@
 // JavaScript to handle slider thumb position and icon changes
 const slider = document.querySelector(".slider");
+const sliderValue = document.getElementById("sliderValue");
 const icons = document.querySelectorAll(".icon");
+
+const moveSlider = (position) => {
+  slider.value = position;
+  // fire input event to update sliderValue
+  slider.dispatchEvent(new Event("input"));
+};
 
 slider.addEventListener("input", function () {
   icons.forEach((icon) => icon.classList.remove("active"));
   const selectedIcon = icons[slider.value];
   selectedIcon.classList.add("active");
+  switch (this.value) {
+    case "0":
+      sliderValue.textContent = "speed";
+      break;
+    case "1":
+      sliderValue.textContent = "balanced";
+      break;
+    case "2":
+      sliderValue.textContent = "accuracy";
+      break;
+    default:
+      sliderValue.textContent = "balanced";
+  }
+});
+
+icons.forEach((icon) => {
+  icon.addEventListener("click", function () {
+    icons.forEach((icon) => icon.classList.remove("active"));
+    this.classList.add("active");
+    switch (this.id) {
+      case "speed":
+        moveSlider(0);
+        break;
+      case "balanced":
+        moveSlider(1);
+        break;
+      case "accuracy":
+        moveSlider(2);
+        break;
+      default:
+        moveSlider(1);
+    }
+  });
 });
