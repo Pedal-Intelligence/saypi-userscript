54c0ca0ed283e0b9aec45d63d1331579afc49c25
Ross Cadogan <ross.cadogan@gmail.com>
2024-05-05 21:52:06 +0100

    Mock `chrome` APIs for Jest in jest.setup.js

--- test/jest.setup.js
+++ test/jest.setup.js
@@ -1,3 +1,28 @@
 process.env.GA_MEASUREMENT_ID = "mock-measurement-id";
 process.env.GA_API_SECRET = "mock-api-secret";
 process.env.GA_ENDPOINT = "https://www.google-analytics.com/mp/collect";
+
+// Mock `chrome` APIs for Jest
+if (typeof chrome === "undefined") {
+  const storage = {};
+  global.chrome = {
+    storage: {
+      sync: {
+        get: (keys, callback) => {
+          // Mock implementation with in-memory storage
+          return storage[keys];
+        },
+        set: (items, callback) => {
+          // Mock implementation with in-memory storage
+          storage = { ...storage, ...items };
+          return callback();
+        },
+      },
+    },
+    runtime: {
+      onMessage: {
+        addListener: () => {},
+      },
+    },
+  };
+}
