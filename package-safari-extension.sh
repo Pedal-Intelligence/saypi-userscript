6f74761973d22d3f8cf3cabb5b2cdf554f01a7bf
Ross Cadogan <ross.cadogan@gmail.com>
2024-09-22 00:20:43 +0100

    Cross-device package script. Cursor seems cool.

--- package-safari-extension.sh
+++ package-safari-extension.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
 ./package-chrome-extension.sh
-unzip -o -d "../saypi-safari/Say, Pi/Shared (Extension)/Resources/" dist/saypi.chrome.zip
+SAFARI_BASE_DIR="../saypi-safari"
+if [ -d "$SAFARI_BASE_DIR/Say, Pi" ]; then
+    SAFARI_DIR="$SAFARI_BASE_DIR/Say, Pi"
+else
+    SAFARI_DIR="$SAFARI_BASE_DIR"
+fi
+
+if [ ! -d "$SAFARI_DIR" ]; then
+    echo "Error: Safari directory not found. Please clone the saypi-safari repo first."
+    exit 1
+fi
+
+SAFARI_RESOURCES_DIR="$SAFARI_DIR/Shared (Extension)/Resources/"
+unzip -o -d "$SAFARI_RESOURCES_DIR" dist/saypi.chrome.zip
 echo "Safari repo updated. Now use Xcode to build and archive the Safari extension."
