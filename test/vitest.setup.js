9549e6fc71733b4386bdd4cd88dfadb21aedc6e5
Ross Cadogan <ross.cadogan@gmail.com>
2024-11-03 10:16:55 +0000

    Global JSDOM configuration for vitests
    - This configuration sets up JSDOM for svg support

--- test/vitest.setup.js
+++ test/vitest.setup.js
@@ -1,5 +1,23 @@
 // setup file for vitest - applied before testing *.spec.ts files
 import { vi } from "vitest";
+import { JSDOM } from 'jsdom';
+
+// JSDOM Setup
+const dom = new JSDOM('<!DOCTYPE html>', {
+  url: 'http://localhost',
+  pretendToBeVisual: true
+});
+
+Object.defineProperties(global, {
+  document: { value: dom.window.document },
+  window: { value: dom.window },
+  MutationObserver: { value: dom.window.MutationObserver },
+  Node: { value: dom.window.Node },
+  NodeList: { value: dom.window.NodeList },
+  Element: { value: dom.window.Element },
+  DOMParser: { value: dom.window.DOMParser },
+  SVGElement: { value: dom.window.SVGElement },
+});
 
 const mockStorage = (() => {
   let storage = {};
