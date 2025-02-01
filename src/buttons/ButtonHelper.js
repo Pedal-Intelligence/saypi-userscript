0cfbc8208c137bb3c1945f78d4475f209cb78664
john <johncrawley1@gmail.com>
2025-02-01 03:25:44 +0000

    Updated ButtonHelper to only configure listeners once, instead of every update. Newly added touch listeners have also been added, but 'touchend' is only detected on every other press.

--- src/buttons/ButtonHelper.js
+++ src/buttons/ButtonHelper.js
@@ -5,6 +5,7 @@ import { createSVGElement } from "../dom/DOMModule.ts";
 class ButtonUpdater{
 
     constructor() {
+        this.longPressMilliseconds = 500;
         this.clickStartTime = 0;
         this.isLongClickEngaged = false;      
         this.isMouseDown = false;
@@ -13,6 +14,13 @@ class ButtonUpdater{
 
         // track whether a call is active, so that new button instances can be initialized correctly
         this.callIsActive = false;
+
+        this.currentOnClick = () => {};
+        this.currentOnLongClick = () => {};
+        this.currentOnLongRelease = () => {};
+
+        this.isMouseOutHandled = false;
+        this.areButtonClicksConfigured = false;
     }
 
     removeChildrenFrom(callButton) {
@@ -39,59 +47,92 @@ class ButtonUpdater{
         return runnable ? runnable : () => {};
     }
 
-    isShortClick(clickStartTime, limit) {
-        const clickDuration =  (Date.now() - clickStartTime);
-        return clickStartTime == 0 || clickDuration < limit;
+    isShortClick() {
+        const clickDuration =  (Date.now() - this.clickStartTime);
+        return this.clickStartTime == 0 || clickDuration < this.longPressMilliseconds;
     }
 
     createEvent(eventName) {
-      return eventName ? () => { 
-        this.sayPiActor.send(eventName); 
-      } : null;
+      return eventName 
+        ? () => { this.sayPiActor.send(eventName); }
+        : () => {};
     }
 
     resetListenersOf(button) {
         button.onclick = () => {};
         button.addEventListener("mouseout", () => {});
+        button.addEventListener("touchstart", ()=> {});
+        button.addEventListener("touchend", ()=> {});
+        button.addEventListener("touchcancel", ()=> {});
     }
 
-    handleButtonClicks(options) {
-        let { button, onClick, onLongPressDown, onLongPressUp, isMouseOutHandled = false } = options;
-        const longPressMinimumMilliseconds = 500;
-            
-        onClick = this.setEmptyDefault(onClick);
-        onLongPressDown = this.setEmptyDefault(onLongPressDown);
-        onLongPressUp = this.setEmptyDefault(onLongPressUp);
-
-        button.onmousedown = () => {
-            this.isMouseDown = true;
-            this.clickStartTime = Date.now();
-            window.setTimeout( () => {
-            if (this.isMouseDown === true) {
-                onLongPressDown();
-                this.isLongClickEngaged = true;
-            }
-            }, longPressMinimumMilliseconds);
+    onDown() {
+        this.isMouseDown = true;
+        this.clickStartTime = Date.now();
+        window.setTimeout( () => {
+        if (this.isMouseDown === true) {
+            this.currentOnLongClick();
+            this.isLongClickEngaged = true;
         }
+        }, this.longPressMilliseconds);
+    }
 
-        button.onmouseup = () => { 
-            this.isMouseDown = false;
-            if (this.isShortClick(this.clickStartTime, longPressMinimumMilliseconds)) {
-                onClick();
-            } else if (this.isLongClickEngaged) {
-                onLongPressUp();
-                this.isLongClickEngaged = false;
-            }
+    onUp() {
+        this.isMouseDown = false;
+        if (this.isShortClick()) {
+            this.currentOnClick();
+        } else if (this.isLongClickEngaged) {
+            this.currentOnLongRelease();
+            this.isLongClickEngaged = false;
         }
+    }
 
-        if (isMouseOutHandled) {
-            button.addEventListener("mouseout", () => {
-            if (this.isLongClickEngaged) {
-                onLongPressUp();
-                this.isLongClickEngaged = false;
-            }
+    onOut() {
+        if(this.isMouseOutHandled && this.isLongClickEngaged){
+            this.currentOnLongRelease();
+            this.isLongClickEngaged = false;
+        }
+    }
+
+    setupButtonListeners(button) {
+        if(typeof(window.ontouchstart) != 'undefined'){
+            this.log("setupButtonListeners, onTouchStart is not null, defining touchstart and touchend event listeners");
+            button.addEventListener('touchstart',() => {
+                this.log("ontouchstart!");
+                this.onDown();
             });
+            button.addEventListener('touchend', () => {
+                this.log("ontouchend!");
+                this.onUp();
+            });
+            button.addEventListener('touchcancel', () => {
+                this.log("ontouchcancel!!!");
+                this.onUp();
+            });
+        } else {
+            this.log("ontouchstart could not be found, so defining onmousedown and onmouseup");
+            button.onmousedown = () => this.onDown();
+            button.onmouseup = () => this.onUp();
+            button.addEventListener("mouseout", () => this.onOut());
         }
+    }
+
+    updateButtonRoles(options) {
+        let { button, clickEventName, longDownEventName, longUpEventName, isMouseOutHandled = false } = options;
+                  
+        if(this.areButtonClicksConfigured === false){
+            this.setupButtonListeners(button);
+            this.areButtonClicksConfigured = true;
+        }
+
+        this.currentOnClick = this.createEvent(clickEventName);
+        this.currentOnLongClick = this.createEvent(longDownEventName);
+        this.currentOnLongRelease = this.createEvent(longUpEventName);
+        this.isMouseOutHandled = isMouseOutHandled;
+    }
+
+    log(msg) {
+        console.log("^^^ ButtonHelper: " + msg);
     }
 
     isCallActive() {
@@ -106,13 +147,13 @@ class ButtonUpdater{
         if (button) {
             this.removeChildrenFrom(button);
             this.addIconTo(button, icon);
-            this.resetListenersOf(button);
+           // this.resetListenersOf(button);
             this.setAriaLabelOf(button, label, labelArgument);
-            this.handleButtonClicks({
+            this.updateButtonRoles({
                 button: button,
-                onClick: this.createEvent(clickEventName),
-                onLongPressDown: this.createEvent(longPressEventName),
-                onLongPressUp: this.createEvent(longReleaseEventName),
+                clickEventName: clickEventName,
+                longDownEventName: longPressEventName,
+                longUpEventName: longReleaseEventName,
                 isMouseOutHandled: isMouseOutProcessed,
             });
             this.toggleActiveState(button, isCallActive);
